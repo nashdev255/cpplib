eaa14997e9a70ae6e10d622a0d27bc6b91a283ca
nashdev255 <yutori1107@gmail.com>
2023-10-16 22:49:10 +0900

    add Vector2d::operator+(v:Vector2d&)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #ifndef MAIN_CPP
 #define MAIN_CPP
 
+#include <iostream>
 #include "Vector2d.hpp"
 
 int main(int numberOfArgs, char* argv[]) {
   Vector2d v1(1, 2);
   Vector2d v2(3, 4);
-  Vector2d v();
+  Vector2d v;
 
   v = v1 + v2;
+
+  std::cout << v << std::endl;
 
   return 0;
 }
