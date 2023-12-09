a2d4826c6d802a52ce88ec5c1fc95cf8f939a1a0
nashdev255 <nashdev255@gmail.com>
2023-12-09 20:51:47 +0900

    Alias declaration for int, float, double

--- Vector2.h
+++ Vector2.h
@@ -12,9 +12,14 @@ namespace nash {
       constexpr Vector2(T x, T y);
 
       template <typename U>
-      constexpr Vector2(const Vector2<U>& vec);
+      constexpr explicit Vector2(const Vector2<U>& vec);
 
       virtual ~Vector2();
 
   };
+
+  using Vector2i = Vector2<int>;
+  using Vector2f = Vector2<float>;
+  using Vector2d = Vector2<double>;
+  
 }
